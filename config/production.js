586bc192837ec0528ac0d0e71a2211cf30d89a06
Nádia Fernandes <nadiafernandes@Nadias-MacBook-Pro.local>
2016-05-28 11:57:02 +0200

    new mongo connection

--- config/production.js
+++ config/production.js
@@ -30,7 +30,7 @@ module.exports = {
         testRecipient: 'nadia.fernandess@gmail.com'
     },
     mongodb: {
-        database: 'mongodb://127.0.0.1:27017/email',
+        database: 'mongodb://heroku_dh5n5112:a7dr0ri5gku69md5fstgd8sdlk@ds019053.mlab.com:19053/heroku_dh5n5112/email',
         emailCollection: 'emailQueue',
     },
 };
