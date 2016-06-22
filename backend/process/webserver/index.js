1ebfbb5d36d3531f1c9b9685fe3b87c5689ea3b8
Nádia Fernandes <nadiafernandes@Nadias-MacBook-Pro.local>
2016-06-22 12:32:22 +0200

    previous stratagy does not work

--- backend/process/webserver/index.js
+++ backend/process/webserver/index.js
@@ -10,8 +10,7 @@ var express = require('express'),
     bunyan = require('bunyan'),
     log = bunyan.createLogger({name: 'email-processor', src: true, hostname: ' '}),
     passport = require('passport'),
-    BasicStrategy = require('passport-http').BasicStrategy,
-    mailer = require("../mailer").start(mongodb);
+    BasicStrategy = require('passport-http').BasicStrategy;
 
 passport.use(new BasicStrategy({},
     function (username, password, done) {
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 switch (process.argv[2]) {
     case 'mailer':
-        require('./backend/process/mailer').start();
+        require('./backend/process/mailer');
     default:
         require('./backend/process/webserver');
 }
